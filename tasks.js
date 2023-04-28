88515392d4eaa84e5724d062bc2261f4fc2a08dc
Narasimman2067 <narasimmamech2067@gmail.com>
2023-04-28 20:31:56 +0530

    day 38 task

---
+++ tasks.js
@@ -0,0 +1,38 @@
+const data=[
+{
+    "name":"raja",
+    "task":"product",
+    "topics":"mongodb",
+    "attendance":"100%",
+    "date":"15-oct-2020"
+},
+{
+    "name":"rajaprasanth",
+    "task":"users",
+    "topics":"mongodb",
+    "attendance":"90%",
+    "date":"16-oct-2020"
+},
+{
+    "name":"prakash",
+    "task":"codekatta",
+    "topics":"zenclass",
+    "attendance":"70%",
+    "date":"18-oct-2020"
+},
+{
+    "name":"sanjay",
+    "task":"attendance",
+    "topics":"mongodb",
+    "attendance":"80%",
+    "date":"22-oct-2020"
+},
+{
+    "name":"sanjayraja",
+    "task":"product",
+    "topics":"mongodb",
+    "attendance":"60%",
+    "date":"20-oct-2020"
+}
+
+]
